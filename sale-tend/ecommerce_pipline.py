5112ea32db32ac1a8330e7be91ec4db4bd0b36cd
youngyoung0 <taeyoung6873@gmail.com>
2024-07-07 02:23:06 +0900

    init : project init

---
+++ sale-tend/ecommerce_pipline.py
@@ -0,0 +1,12 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from sklearn.preprocessing import StandardScaler
+
+def main():
+    print("전자상거래 데이터 분석 파이프라인을 시작합니다.")
+
+if __name__ == "__main__":
+    main()
+
